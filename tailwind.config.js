0c630d4c6d8bb3237d6a21466bc9dadcce2cc419
227Faddi <falukhouma27@gmail.com>
2024-09-08 14:16:01 -0400

    removed comments on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,11 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'chakra-petch': ['Chakra Petch', 'sans-serif'],  // Define the custom font
+        'chakra-petch': ['Chakra Petch', 'sans-serif'],  
       },
     },
-    // Set the default font family for the entire project
     fontFamily: {
-      sans: ['Chakra Petch', 'sans-serif'],  // Use the custom font as the default sans-serif font
+      sans: ['Chakra Petch', 'sans-serif'],
     },
   },
 
