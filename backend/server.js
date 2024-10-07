734fd0807d9fc5d8e1f39093629f9f56933b5b9b
227Faddi <falukhouma27@gmail.com>
2024-10-07 13:15:15 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,11 @@ app.use(
         origin: process.env.CLIENT_URL,
         methods: 'GET, POST, PUT, DELETE',
         credentials: true,
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            secure: true, //Enable when deployment OR when not using localhost, this wont work without https
+            sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
+        }
     })
 )
 
