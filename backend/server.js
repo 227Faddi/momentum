b7c23550698eb19a04743616797640d7878656a8
227Faddi <falukhouma27@gmail.com>
2024-10-07 23:43:47 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -37,12 +37,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
-    cookie: {
-        secure: true, //Enable when deployment OR when not using localhost, this wont work without https
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
-    }
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING })
 }))
   
 app.set('trust proxy', 1);
